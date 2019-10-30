716b3fc3fe4dfa1e3c295cca5438413c9b6030ad
Coby Edwards <cobyedwards@Cobys-MacBook-Pro-7.local>
2019-10-30 07:46:27 -0500

    played some catchup, users, layouts, forms, etc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was succesfully updated"
+      flash[:success] = "Article was succesfully updated"
       redirect_to article_path(@article)
     else
       render "edit"
@@ -22,11 +22,11 @@ class ArticlesController < ApplicationController
 
   def create
     # render plain: params[:article].inspect
-    # debugger
+    debugger
     @article = Article.new(article_params)
     @article.user = User.first
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else 
       render 'new'
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
 
